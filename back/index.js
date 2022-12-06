9f3be2d32e6822451d16e7809d16fe6595398a1a
JuanGutiierrez <joselitoguti2003@gmail.com>
2022-12-06 09:30:12 -0500

    creamos carpeta con proyecto react y proyecto node, se hacen las respectivas conecciones a la base de datos por medio de xampp en el proyecto node

--- back/index.js
+++ back/index.js
@@ -1,9 +1,12 @@
+/* Importacion de dependencia y archivos */
 import express from 'express';
 import dataBase  from './database/conexionDatabase.js';
 
+/* Instancia de Expres y apertura de puerto */
 const app = express();
 const port = 3100;
 
+/* Validacion de coneccion a la base de datos */
 try {
     await dataBase.authenticate()
     console.log('Conexion exitosa a la base de datos')
@@ -11,6 +14,7 @@ try {
     console.log('Conexion no exitosa')
 }
 
+/* Ejecucion de servidor */
 app.listen(port, () =>{
     console.log(`Servidor corriendo en el puerto ${port}`)
     console.log(`Servidor corriendo en el puerto http://localhost:3100`)
